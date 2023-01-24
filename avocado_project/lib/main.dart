d82ee9dfdb10ff0dbbf636e3003296a7d1d65825
ianishthakur <ianishthakur@gmail.com>
2023-01-24 12:30:20 +0545

    First commit

--- avocado_project/lib/main.dart
+++ avocado_project/lib/main.dart
@@ -32,14 +32,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,24 +44,6 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
    
-        child: Column(
-  
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
