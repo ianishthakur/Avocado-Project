62304cc001b2240a206192cd4342954ead9ad4ed
ianishthakur <ianishthakur@gmail.com>
2023-01-24 15:06:55 +0545

    App ready to push

--- avocado_project/README.md
+++ avocado_project/README.md
@@ -1,16 +1,16 @@
 # avocado_project
 
-A new Flutter project.
+A new Avocado project.
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+This project is a given by Avocado Technology for the Flutter position.
 
-A few resources to get you started if this is your first Flutter project:
+Design a page as per the Figma design provided in the following link:
+<https://www.figma.com/file/YcEHsy2HGufI0i14TTiHYd/Carousel-Slider-Animation?node-id=0%3A1&t=cBwcYDLZoXtq73YM-0>
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+The deadline for the task is 2 days from today, i.e. January 25, 2023
+before 5:00 PM.
+  
+Here is the screenshort:
+  ![Alt text]("C:\Users\ianis\OneDrive\Desktop\Avocado\Avocado-Project\avocado_project\Screenshot_1674551568.png"?raw=true "Optional Title")
--- avocado_project/lib/ui/home_page.dart
+++ avocado_project/lib/ui/home_page.dart
@@ -20,6 +20,17 @@ class _HomePageState extends State<HomePage> {
     "https://images.wallpapersden.com/image/download/purple-sunrise-4k-vaporwave_bGplZmiUmZqaraWkpJRmbmdlrWZlbWU.jpg",
     "https://wallpaperaccess.com/full/2637581.jpg",
     "https://uhdwallpapers.org/uploads/converted/20/01/14/the-mandalorian-5k-1920x1080_477555-mm-90.jpg"
+        "https://www.dreamworks.com/storage/cms-uploads/puss-in-boots-the-last-wish-key-art2.jpg",
+    "https://parade.com/.image/t_share/MTkwNTc4NzcxNjE5NjIwNzMy/disney-pixar.jpg",
+  ];
+
+  List<String> image = [
+    "https://www.dreamworks.com/storage/cms-uploads/puss-in-boots-the-last-wish-key-art2.jpg",
+    "https://parade.com/.image/t_share/MTkwNTc4NzcxNjE5NjIwNzMy/disney-pixar.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTIZccfNPnqalhrWev-Xo7uBhkor57_rKbkw&usqp=CAU",
+    "https://images.wallpapersden.com/image/download/purple-sunrise-4k-vaporwave_bGplZmiUmZqaraWkpJRmbmdlrWZlbWU.jpg",
+    "https://wallpaperaccess.com/full/2637581.jpg",
+    "https://uhdwallpapers.org/uploads/converted/20/01/14/the-mandalorian-5k-1920x1080_477555-mm-90.jpg"
   ];
   @override
   void initState() {
@@ -27,6 +38,7 @@ class _HomePageState extends State<HomePage> {
     _pageController = PageController(viewportFraction: 0.8, initialPage: 1);
   }
 
+  int ourCurrentIndex = 0;
   Color containerColor = const Color(0xFF05113b);
   Color activeIndexColor = const Color(0xFF4ccdeb);
   Color inActiveIndexColor = const Color(0xFF1a496f);
@@ -34,180 +46,291 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          // toolbarHeight: 9 * 10,
-          // backgroundColor: Colors.white,
-          elevation: 0,
-          leadingWidth: 0,
-          title: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                padding: const EdgeInsets.only(
-                  top: 10,
-                ),
-                child: Row(
-                  children: [
-                    CircleAvatar(
-                      child: Image.asset(Assets.profile),
-                    ),
-                    const SizedBox(width: 10),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        RichText(
-                          text: const TextSpan(
-                            style: TextStyle(
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.w300,
-                              color: Colors.white,
-                            ),
-                            children: <TextSpan>[
-                              TextSpan(text: Strings.hello),
-                              TextSpan(
-                                  text: Strings.arie,
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                      fontSize: 18)),
-                            ],
+      appBar: AppBar(
+        // toolbarHeight: 9 * 10,
+        // backgroundColor: Colors.white,
+        elevation: 0,
+        leadingWidth: 0,
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(
+                top: 10,
+              ),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    child: Image.asset(Assets.profile),
+                  ),
+                  const SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      RichText(
+                        text: const TextSpan(
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white,
                           ),
+                          children: <TextSpan>[
+                            TextSpan(text: Strings.hello),
+                            TextSpan(
+                                text: Strings.arie,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                    fontSize: 18)),
+                          ],
                         ),
-
-                        const SizedBox(
-                          width: 200,
-                          child: Text(Strings.bookYour,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.normal,
-                                  fontSize: 14,
-                                  color: Colors.white)),
-                        ),
-                        // Container(
-                        //   width: 200,
-                        //   child: Text(widget.useremail,
-                        //       style: GoogleFonts.bitter(
-                        //           textStyle: const TextStyle(
-                        //               fontSize: 18, color: Colors.white))),
-                        // ),
+                      ),
+                      const SizedBox(
+                        width: 200,
+                        child: Text(Strings.bookYour,
+                            style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 14,
+                                color: Colors.white)),
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                  CircleAvatar(
+                    radius: 18,
+                    backgroundColor: Colors.white.withOpacity(0.2),
+                    child: const Icon(
+                      Icons.notifications_rounded,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.99,
+          width: double.infinity,
+          color: containerColor,
+          child: Column(children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 18, vertical: 20),
+              height: 50,
+              width: double.infinity,
+              child: TextField(
+                  cursorColor: Colors.white,
+                  autofocus: false,
+                  style: const TextStyle(fontSize: 16.0, color: Colors.white),
+                  decoration: InputDecoration(
+                    filled: true,
+                    prefixIcon: const Icon(
+                      Icons.search_sharp,
+                      color: Colors.white,
+                      size: 32,
+                    ),
+                    suffixIcon: const Icon(
+                      Icons.tune_rounded,
+                      color: Colors.white,
+                      size: 32,
+                    ),
+                    fillColor: Colors.white.withOpacity(0.2),
+                    hintText: Strings.searchMovie,
+                    hintStyle: const TextStyle(
+                        color: Colors.white, fontSize: 16, height: 1.8),
+                    contentPadding: const EdgeInsets.only(
+                        left: 14.0, bottom: 8.0, top: 8.0),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.7),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.7),
+                    ),
+                  ),
+                  controller: _searchController),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                children: [
+                  RichText(
+                    text: const TextSpan(
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: Strings.now,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 22)),
+                        TextSpan(
+                            text: Strings.showing,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w300,
+                                color: Colors.white,
+                                fontSize: 22)),
                       ],
                     ),
-                    const Spacer(),
-                    CircleAvatar(
-                      radius: 18,
-                      backgroundColor: Colors.white.withOpacity(0.2),
-                      child: const Icon(
-                        Icons.notifications_rounded,
+                  ),
+                  const Spacer(),
+                  const Text(
+                    Strings.seeMore,
+                    style: TextStyle(color: Colors.grey),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              height: MediaQuery.of(context).size.height * 0.4,
+              child: PageView.builder(
+                  itemCount: images.length,
+                  pageSnapping: true,
+                  controller: _pageController,
+                  onPageChanged: (page) {
+                    setState(() {
+                      activePage = page;
+                    });
+                  },
+                  itemBuilder: (context, pagePosition) {
+                    bool active = pagePosition == activePage;
+                    return slider(images, pagePosition, active);
+                  }),
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: indicators(images.length, activePage)),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+              child: Row(
+                children: [
+                  RichText(
+                    text: const TextSpan(
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w300,
                         color: Colors.white,
                       ),
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: Strings.animation,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 22)),
+                        TextSpan(
+                            text: Strings.film,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w300,
+                                color: Colors.white,
+                                fontSize: 22)),
+                      ],
                     ),
-                  ],
+                  ),
+                  const Spacer(),
+                  const Text(
+                    Strings.seeMore,
+                    style: TextStyle(color: Colors.grey),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              height: MediaQuery.of(context).size.height * 0.3,
+              child: ListView.separated(
+                separatorBuilder: (context, index) => const SizedBox(
+                  width: 12,
                 ),
-              ),
-            ],
-          ),
-        ),
-        body: SingleChildScrollView(
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            width: double.infinity,
-            color: containerColor,
-            child: Column(children: [
-              Container(
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 18, vertical: 20),
-                height: 50,
-                width: double.infinity,
-                child: TextField(
-                    cursorColor: Colors.white,
-                    autofocus: false,
-                    style: const TextStyle(fontSize: 16.0, color: Colors.white),
-                    decoration: InputDecoration(
-                      filled: true,
-                      prefixIcon: const Icon(
-                        Icons.search_sharp,
-                        color: Colors.white,
-                        size: 32,
-                      ),
-                      suffixIcon: const Icon(
-                        Icons.tune_rounded,
-                        color: Colors.white,
-                        size: 32,
-                      ),
-                      fillColor: Colors.white.withOpacity(0.2),
-                      hintText: Strings.searchMovie,
-                      hintStyle: const TextStyle(
-                          color: Colors.white, fontSize: 16, height: 1.8),
-                      contentPadding: const EdgeInsets.only(
-                          left: 14.0, bottom: 8.0, top: 8.0),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.7),
-                      ),
-                      enabledBorder: UnderlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.7),
-                      ),
-                    ),
-                    controller: _searchController),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Row(
-                  children: [
-                    RichText(
-                      text: const TextSpan(
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.w300,
-                          color: Colors.white,
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: image.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final item = image[index];
+                  return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 180,
+                          width: 140,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                          child: Image.network(
+                            item,
+                            fit: BoxFit.fill,
+                          ),
                         ),
-                        children: <TextSpan>[
-                          TextSpan(
-                              text: Strings.now,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                  fontSize: 22)),
-                          TextSpan(
-                              text: Strings.showing,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w300,
-                                  color: Colors.white,
-                                  fontSize: 22)),
-                        ],
-                      ),
-                    ),
-                    const Spacer(),
-                    const Text(
-                      Strings.seeMore,
-                      style: TextStyle(color: Colors.grey),
+                      ]);
+                },
+              ),
+            ),
+          ]),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: containerColor,
+        onTap: (index) {
+          ourCurrentIndex = index;
+          setState(() {});
+        },
+        currentIndex: ourCurrentIndex,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+              icon: ourCurrentIndex == 0
+                  ? const Icon(
+                      Icons.home,
+                      color: Colors.white,
                     )
-                  ],
-                ),
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: 350,
-                child: PageView.builder(
-                    itemCount: images.length,
-                    pageSnapping: true,
-                    controller: _pageController,
-                    onPageChanged: (page) {
-                      setState(() {
-                        activePage = page;
-                      });
-                    },
-                    itemBuilder: (context, pagePosition) {
-                      bool active = pagePosition == activePage;
-                      return slider(images, pagePosition, active);
-                    }),
-
-//
-              ),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: indicators(images.length, activePage))
-            ]),
-          ),
-        ));
+                  : const Icon(
+                      Icons.home,
+                      color: Colors.grey,
+                    ),
+              label: ""),
+          BottomNavigationBarItem(
+              icon: ourCurrentIndex == 1
+                  ? const Icon(
+                      Icons.grid_goldenratio_rounded,
+                      color: Colors.white,
+                    )
+                  : const Icon(
+                      Icons.grid_goldenratio_rounded,
+                      color: Colors.grey,
+                    ),
+              label: ""),
+          BottomNavigationBarItem(
+              icon: ourCurrentIndex == 2
+                  ? const Icon(
+                      Icons.account_balance_wallet_sharp,
+                      color: Colors.white,
+                    )
+                  : const Icon(
+                      Icons.account_balance_wallet_sharp,
+                      color: Colors.grey,
+                    ),
+              label: ""),
+          BottomNavigationBarItem(
+              icon: ourCurrentIndex == 3
+                  ? const Icon(
+                      Icons.account_circle,
+                      color: Colors.white,
+                    )
+                  : const Icon(
+                      Icons.account_circle,
+                      color: Colors.grey,
+                    ),
+              label: ""),
+        ],
+      ),
+    );
   }
 
   List<Widget> indicators(imagesLength, currentIndex) {
